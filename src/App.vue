0bccc25dbdf0688b91ebc5dce98c53dbe5d441ee
Sergey Minenkov <minenkov.sergey42@gmail.com>
2021-01-26 15:07:24 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="lightBox">
+    <Red class="light" />
+    <Yellow class="light" />
+    <Green class="light" />
+  </div>
+</template>
+
+<script>
+import Red from "./components/Red.vue";
+import Yellow from "./components/Yellow.vue";
+import Green from "./components/Green.vue";
+
+export default {
+  name: "App",
+  components: {
+    Red,
+    Yellow,
+    Green
+  },
+  data() {
+    return {
+      current: "",
+      previous: ""
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.lightBox {
+  width: 70px;
+  height: 200px;
+  background-color: black;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
